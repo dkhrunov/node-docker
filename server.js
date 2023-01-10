260e0e59a27e041a14f332dadc4a730ba2e86a0d
Denis Khrunov <dkhrunov@t1-consulting.ru>
2023-01-11 02:00:44 +0300

    Added mongodb and configure app with docker-compose with debugging

--- server.js
+++ server.js
@@ -1,9 +1,28 @@
 const server = require('ronin-server')
+const database = require('ronin-database')
 const mocks = require('ronin-mocks')
 
-const app = server.server()
+async function main() {
+  try {
+    await database.connect(process.env.DB_CONNECTION)
 
-app.use('/', mocks.server(app.Router(), false, true))
-app.start()
+    const app = server.server({
+      port: process.env.PORT
+    })
 
-console.log(`Started as ${process.env.NODE_ENV}`)
+    app.use('/foo', (req, res) => {
+      return res.json({ "foo": "bar" })
+    })
+
+    app.use('/', mocks.server(app.Router()))
+
+    const result = await app.start()
+
+    console.info(result)
+    console.log(`Started as ${process.env.NODE_ENV}`)
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+main()
